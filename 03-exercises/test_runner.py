6a8cc079bbbb9970cbd925b436c3239600fc0942
yaojenkuo <tonykuoyj@gmail.com>
2021-11-30 22:31:22 +0800

    update

--- 03-exercises/test_runner.py
+++ 03-exercises/test_runner.py
@@ -3,29 +3,43 @@ import ipynb.fs.full.exercises as ex
 import numpy as np
 import pandas as pd
 
-class TestExercisesFour(unittest.TestCase):
-    def test_01_predict_survived_chimp_model(self):
-        survived_chimp_model = ex.predict_survived_chimp_model()
-        self.assertIsInstance(survived_chimp_model, np.ndarray)
-        self.assertEqual(survived_chimp_model.shape, (418,))
-    def test_02_predict_survived_expert_model(self):
-        survived_expert_model = ex.predict_survived_expert_model()
-        self.assertIsInstance(survived_expert_model, np.ndarray)
-        self.assertEqual(survived_expert_model.shape, (418,))
-    def test_03_predict_survived_sklearn_model(self):
-        survived_sklearn_model = ex.predict_survived_sklearn_model()
-        self.assertIsInstance(survived_sklearn_model, np.ndarray)
-        np.testing.assert_almost_equal(survived_sklearn_model,
-                                      np.array([ 4.73195566, -1.16846277, -2.61196369, -0.03342722]))
-    def test_04_predict_survived_gradient_descent_model(self):
-        survived_gradient_descent_model = ex.predict_survived_gradient_descent_model()
-        self.assertIsInstance(survived_gradient_descent_model, np.ndarray)
-        self.assertEqual(survived_gradient_descent_model.size, (4,))
-        self.assertTrue(survived_gradient_descent_model[1] < 0)
-        self.assertTrue(survived_gradient_descent_model[2] < 0)
-        self.assertTrue(survived_gradient_descent_model[3] < 0)
+class TestExercisesThree(unittest.TestCase):
+    def test_01_predict_sale_price_chimp_model(self):
+        sale_price_chimp_model = ex.predict_sale_price_chimp_model()
+        self.assertIsInstance(sale_price_chimp_model, np.ndarray)
+        self.assertEqual(sale_price_chimp_model.shape, (1459,))
+    def test_02_predict_sale_price_expert_model(self):
+        sale_price_expert_model = ex.predict_sale_price_expert_model()
+        self.assertIsInstance(sale_price_expert_model, np.ndarray)
+        self.assertEqual(sale_price_expert_model.shape, (1459,))
+    def test_03_predict_sale_price_sklearn_model(self):
+        sale_price_sklearn_model = ex.predict_sale_price_sklearn_model()
+        self.assertIsInstance(sale_price_sklearn_model, np.ndarray)
+        self.assertEqual(sale_price_sklearn_model.size, 2)
+        self.assertTrue(sale_price_sklearn_model[0] > 0)
+        self.assertTrue(sale_price_sklearn_model[1] > 0)
+    def test_04_predict_sale_price_normal_equation_model(self):
+        sale_price_normal_equation_model = ex.predict_sale_price_normal_equation_model()
+        self.assertIsInstance(sale_price_normal_equation_model, np.ndarray)
+        self.assertEqual(sale_price_normal_equation_model.size, 2)
+        self.assertTrue(sale_price_normal_equation_model[0] > 0)
+        self.assertTrue(sale_price_normal_equation_model[1] > 0)
+    def test_05_predict_height_meters_sklearn_model(self):
+        height_meters_sklearn_model = ex.predict_height_meters_sklearn_model()
+        self.assertIsInstance(height_meters_sklearn_model, np.ndarray)
+        self.assertEqual(height_meters_sklearn_model.size, 3)
+        self.assertTrue(height_meters_sklearn_model[0] > 0)
+        self.assertTrue(height_meters_sklearn_model[1] < 0)
+        self.assertTrue(height_meters_sklearn_model[2] > 0)
+    def test_06_predict_height_meters_gradient_descent_model(self):
+        height_meters_gradient_descent_model = ex.predict_height_meters_gradient_descent_model()
+        self.assertIsInstance(height_meters_gradient_descent_model, np.ndarray)
+        self.assertEqual(height_meters_gradient_descent_model.size, 3)
+        self.assertTrue(height_meters_gradient_descent_model[0] > 0)
+        self.assertTrue(height_meters_gradient_descent_model[1] < 0)
+        self.assertTrue(height_meters_gradient_descent_model[2] > 0)
 
-suite = unittest.TestLoader().loadTestsFromTestCase(TestExercisesFour)
+suite = unittest.TestLoader().loadTestsFromTestCase(TestExercisesThree)
 runner = unittest.TextTestRunner(verbosity=2)
 if __name__ == '__main__':
     test_results = runner.run(suite)
